96eb0948dfaf0a0d89a53abc74a720c46594a39d
Yuxiang-prog <85142336+Yuxiang-prog@users.noreply.github.com>
2021-09-10 17:03:34 -0400

    Update WhiteSheets.java

--- WhiteSheets.java
+++ WhiteSheets.java
@@ -12,7 +12,7 @@ import java.util.StringTokenizer;
 
 public class WhiteSheets {
     public static void main(String[] args) throws Exception {
-        Kattio in = new Kattio();
+        Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int x1, y1, x2, y2;
         x1 = in.nextInt();
@@ -46,59 +46,5 @@ public class WhiteSheets {
 
     static long getArea(Rectangle r) {
         return r.isEmpty() ? 0 : (long) r.getHeight() * (long) r.getWidth();
-    }
-    static class Kattio extends PrintWriter {
-        private BufferedReader r;
-        private StringTokenizer st;
-
-        // standard input
-        public Kattio() {
-            this(System.in, System.out);
-        }
-
-        public Kattio(InputStream i, OutputStream o) {
-            super(o);
-            r = new BufferedReader(new InputStreamReader(i));
-        }
-
-        // USACO-style file input
-        public Kattio(String problemName) throws IOException {
-            super(new FileWriter(problemName + ".out"));
-            r = new BufferedReader(new FileReader(problemName + ".in"));
-        }
-
-        // returns null if no more input
-        public String next() {
-            try {
-                while (st == null || !st.hasMoreTokens())
-                    st = new StringTokenizer(r.readLine());
-                return st.nextToken();
-            } catch (Exception e) {
-            }
-            return null;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        public String readLine() throws Exception {
-            return r.readLine();
-        }
-
-        public long[] readArray(int n) {
-            long[] arr = new long[n];
-            for (int i = 0; i < n; i++)
-                arr[i] = nextLong();
-            return arr;
-        }
     }
 }
